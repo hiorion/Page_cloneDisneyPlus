24a22e595d222d90130bbeb05750e6cad8297c5c
henry orion <hen.orion@gmail.com>
2025-05-28 06:32:50 -0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
-function styles(){
+
+
+function styles() {
     return gulp.src("./src/styles/*.scss")
-        .pipe(sass({ output: "compressed"}))
+        .pipe(sass({ outputStyle: "compressed" }))
         .pipe(gulp.dest("dist/css"));
 }
 
-exports.default = styles;
-exports.watch = function (){
-    gulp.watch("./src/styles/*.scss", gulp.parallel(styles))
+function image() {
+  return gulp.src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images"));
 }
+
+exports.default = gulp.parallel(styles, image);
+
+exports.watch = function () {
+    gulp.watch("./src/styles/*.scss", styles);
+};
